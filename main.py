4f4e1c0d0524673e89f5807ddbc09990cbabf832
Cheung <cheung@tdfcu.com>
2022-10-19 14:27:00 -0400

    added bounce function

--- main.py
+++ main.py
@@ -26,4 +26,9 @@ while game_is_on:
     screen.update()
     ball.movement()
 
+    # Detection collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+
+
 screen.exitonclick()
